ae1eb6fb346762a166d397bbd33d4947775c5065
win-m <win-m@DESKTOP-K1SBAGL>
2023-08-10 16:56:19 +0200

    Add project files.

---
+++ Appliction/Abstractions/IPostRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appliction.Abstractions
+{
+    public interface IPostRepository
+    {
+
+        Task<ICollection<Post>> GetllPosts();
+     
+        Task<Post> GetPostById(int postId);
+
+        Task<Post> CreatePost(Post toCreate);
+
+        Task<Post> UpdatePost(string updateContent, int postId);
+
+        Task<Post> DeletePost(int postId);
+    }
+}
---
+++ DataAccess/Repositories/PostRepository.cs
@@ -0,0 +1,38 @@
+﻿using Appliction.Abstractions;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        public Task<Post> CreatePost(Post toCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> DeletePost(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Post>> GetllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPostById(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> UpdatePost(string updateContent, int postId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
