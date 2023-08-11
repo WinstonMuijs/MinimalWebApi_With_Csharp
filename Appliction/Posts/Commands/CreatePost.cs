498856d758f4292a0fed1e874b5d626eef8405b8
WinstonMuijs <whmuijs@gmail.com>
2023-08-11 14:12:50 +0200

    Migratie, updated Database, MediatoR toegevoegd, folders gemaakt voor commando's en e afhandeling van verzoeken.

---
+++ Appliction/Posts/Commands/CreatePost.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using MediatR;
+
+
+namespace Appliction.Posts.Commands
+{
+    public class CreatePost : IRequest<Post>
+    {
+        public string? PostContant { get; set; }
+    }
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,13 @@
+using Appliction.Abstractions;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Windows.Markup;
+
 var builder = WebApplication.CreateBuilder(args);
+var cs = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(cs));
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 
 // Add services to the container.
 
