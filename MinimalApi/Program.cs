6c7d5ac1dd948af7391bc10cb12db05406fcfba6
WinstonMuijs <whmuijs@gmail.com>
2023-08-13 17:05:54 +0200

    Update

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -10,14 +10,18 @@ using Microsoft.EntityFrameworkCore;
 using System.Windows.Markup;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var cs = builder.Configuration.GetConnectionString("Default");
+
 builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(cs));
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddMediatR(typeof(CreatePost));
 
+
 // Add services to the container.
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 
